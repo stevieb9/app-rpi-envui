1cd6be81daaaaf1c2ff1ea5e18f5f1fc16a2ad00
Steve Bertrand <steveb@cpan.org>
2016-09-26 12:54:33 -0600

    we now use the db to store the details of each auxillary

--- lib/App/EnvUI.pm
+++ lib/App/EnvUI.pm
@@ -9,18 +9,14 @@ use JSON::XS;
 
 our $VERSION = '0.1';
 
-my $conf = parse_config();
-
-my $auxs;
-$auxs = _generate_aux();
-tie $auxs, 'IPC::Shareable', undef, {destroy => 1};
+parse_config();
 
 my $event_env_to_db = Async::Event::Interval->new(
     5,
     sub {
         my $temp = int(rand(100));
         my $humidity = int(rand(100));
-        insert($temp, $humidity);
+        db_insert_env($temp, $humidity);
     }
 );
 
@@ -28,15 +24,13 @@ my $event_action_env = Async::Event::Interval->new(
     2,
     sub {
         my $env = fetch_env();
-        print "*** $auxs->{aux3}{pin}, $auxs->{aux3}{state} ***\n";
-    }
+    },
 );
 
 $event_env_to_db->start;
 $event_action_env->start;
 
 get '/' => sub {
-        $auxs = _generate_aux();
         return template 'main';
 
         # the following events have to be referenced to within a route.
@@ -49,29 +43,31 @@ get '/' => sub {
 
 get '/get_aux/:aux' => sub {
         my $aux = params->{aux};
-        my $state = _aux_state($aux);
-        my $override = _aux_override($aux);
-        my $pin = _aux_pin($aux);
+        my $state = aux_state($aux);
+        my $override = aux_override($aux);
+        my $pin = aux_pin($aux);
+
+        print "!!! $aux, $state, $override, $pin\n";
         return to_json {
                 pin => $pin,
                 state => $state,
                 override => $override,
-                pin => $auxs->{$aux}{pin},
+                pin => $pin,
         };
     };
 
 get '/set_aux/:aux/:state' => sub {
         my $aux = params->{aux};
+
         my $state = _bool(params->{state});
+        $state = aux_state($aux, $state);
 
-        _aux_state($aux, $state);
-
-        my $override = _aux_override($aux) ? 0 : 1;
-        _aux_override($aux, $override);
+        my $override = aux_override($aux) ? 0 : 1;
+        $override = aux_override($aux, $override);
 
         return to_json {
             aux => $aux,
-            state => $auxs->{$aux}{state}
+            state => $state,
         };
     };
 
@@ -83,7 +79,7 @@ get '/fetch_env' => sub {
         };
     };
 
-sub insert {
+sub db_insert_env {
     my ($temp, $hum) = @_;
     database->quick_insert(stats => {
             temp => $temp,
@@ -106,45 +102,41 @@ sub _bool {
     my $bool = shift;
     return $bool eq 'true' ? 1 : 0;
 }
-sub _aux_state {
+sub aux_state {
     # maintains the auxillary state (on/off)
 
     my ($aux, $state) = @_;
-    return $auxs->{$aux}{state} if ! defined $state;
-    $auxs->{$aux}{state} = $state;
-    return $state;
+    if (defined $state){
+        update_aux_db('state', $aux, $state);
+    }
+    return fetch_aux($aux)->{state};
 }
-sub _aux_override {
+sub aux_override {
     # sets a manual override flag if an aux is turned on manually (via button)
 
     my ($aux, $override) = @_;
-    return $auxs->{$aux}{override} if ! defined $override;
-    $auxs->{$aux}{override} = $override;
-    return $override;
+
+    if (defined $override){
+        update_aux_db('override', $aux, $override);
+    }
+    return fetch_aux($aux)->{override};
 }
-sub _aux_pin {
+sub aux_pin {
     # returns the auxillary's GPIO pin number
 
-    my $aux = shift;
-    return $auxs->{$aux}{pin};
-}
-sub _generate_aux {
-    # generate the auxillary control objects
+    my ($aux, $pin) = @_;
 
-    my %auxillaries;
-
-    for (1..4){
-        my $aux = "aux$_";
-        $auxillaries{$aux} = {
-            pin => $conf->{$aux}{pin},
-            default => $conf->{$aux}{default},
-            state => 0,
-            on_time => 0,
-            override => 0,
-            name => $aux,
-        };
+    if (defined $pin){
+        update_aux_db('aux', $aux, $pin);
     }
-    return \%auxillaries;
+    return fetch_aux($aux)->{pin};
+}
+sub update_aux_db {
+    my ($col, $aux, $value) = @_;
+    my $table = 'aux';
+
+    database->do("UPDATE $table SET $col='$value' where id='$aux'");
+#    $sth->execute($value, $aux);
 }
 sub _get_last_id {
     my $id = database->selectrow_arrayref(
@@ -160,6 +152,14 @@ sub parse_config {
         $json = <$fh>;
     }
     my $conf = decode_json $json;
-    return $conf;
+
+    for (1..4){
+        my $aux = "aux$_";
+        update_aux_db('pin', $aux, $conf->{$aux}{pin});
+    }
+}
+sub fetch_aux {
+    my $aux = shift;
+    my $aux_obj = database->selectrow_hashref("select * from aux where id='$aux'");
 }
 true;
--- src/db.sql
+++ src/db.sql
@@ -1,12 +1,13 @@
-CREATE TABLE stats (
+CREATE TABLE IF NOT EXISTS stats (
   id integer primary key autoincrement,
   datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
   temp tinyint(3) not NULL,
   humidity tinyint(3) not NULL
 );
 
-CREATE TABLE aux (
-    id VARCHAR(5),
+CREATE TABLE IF NOT EXISTS aux (
+    id VARCHAR(4),
+    pin TINYINT(2),
     state TINYINT(1),
     override TINYINT(1),
     on_time INTEGER
