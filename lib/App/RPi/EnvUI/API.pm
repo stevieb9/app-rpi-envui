0f3afe797a07adc492c2df28bf48f4b03b7088c5
Steve Bertrand <steveb@cpan.org>
2017-11-13 17:22:41 -0800

    events now restart on crash; began changing light timing code

--- lib/App/RPi/EnvUI/API.pm
+++ lib/App/RPi/EnvUI/API.pm
@@ -25,6 +25,10 @@ my $log;
 my $sensor;
 my $events;
 
+my $light_on_time;
+my $light_off_time;
+my $light_initialized = 0;
+
 # public environment methods
 
 sub new {
@@ -110,6 +114,14 @@ sub action_temp {
 sub action_light {
     my ($self, %test_conf) = @_;
 
+    #FIXME: remove set_light_times() from here and docs
+    # - refactor/clean up the two new private methods, and put them into the
+    #   proper location
+    # - modify/remove the light times from the db and db API (and docs)
+    # - clean up this method properly (remove any remnants
+    # - add logging
+    # - figure out better method for class vars that belong in here
+
     my $log = $log->child('action_light');
 
     my $aux      = $self->_config_control('light_aux');
@@ -118,28 +130,65 @@ sub action_light {
 
     return if $override;
 
-    my $on_time  = $self->_config_light('on_time');
-    my $off_time = $self->_config_light('off_time');
-
+    my $on_at = $self->_config_light('on_at');
+    
     my $on_hours = defined $test_conf{on_hours}
         ? $test_conf{on_hours}
         : $self->_config_light('on_hours');
 
-    my $now = defined $test_conf{now} ? $test_conf{now} : time;
+    if (! $light_initialized){
+        my $dt_now = DateTime->now;
+        $dt_now->set_time_zone('local');
 
-    if (($on_hours == 24) || ($now > $on_time && $now < $off_time)){
+        my $dt_on = $dt_now->clone;
+        $dt_on->set_hour((split(/:/, $on_at))[0]);
+        $dt_on->set_minute((split(/:/, $on_at))[1]);
+        $dt_on->set_second(0);
+
+        $light_on_time = [$dt_on->hour, $dt_on->minute, $dt_on->second];
+
+        my $dt_off = $dt_on->clone;
+        $dt_off->add(hours => $on_hours);
+        
+        $light_off_time = [$dt_off->hour, $dt_off->minute, $dt_off->second];
+
+        #printf "on:  h: %d, m: %d\n", $light_on_time->[0], $light_on_time->[1];
+        #printf "off: h: %d, m: %d\n", $light_off_time->[0], $light_off_time->[1];
+
+        $light_initialized = 1;
+    }
+
+    if (($on_hours == 24) || _is_lights_on_time($light_on_time, $light_off_time)){
         if (! $self->aux_state($aux)){
             $self->aux_state($aux, ON);
             pin_mode($pin, OUTPUT);
             write_pin($pin, HIGH);
         }
     }
-    elsif ($self->aux_state($aux)){
+    if ($self->aux_state($aux) && ! _is_lights_on_time($light_on_time, $light_off_time)){
         $self->aux_state($aux, OFF);
         pin_mode($pin, OUTPUT);
         write_pin($pin, LOW);
-        $self->set_light_times;
     }
+}
+sub _is_lights_on_time {
+    my ($on, $off) = @_;
+    my @now = (localtime(time))[2,1,0];
+
+    if (_light_time_compare($on, $off) == 1){
+        return _light_time_compare(\@now, $on) == 1 || _light_time_compare(\@now, $off) == -1;
+    }
+    else {
+        return _light_time_compare(\@now, $on) == 1 && _light_time_compare(\@now, $off) == -1
+    }
+    return 0;
+}    
+sub _light_time_compare {
+    my ($a, $b) = @_;
+    for (0..2){
+        return $a->[$_] <=> $b->[$_] if $a->[$_] != $b->[$_];
+    }
+    return 0;
 }
 sub aux {
     my ($self, $aux_id) = @_;
@@ -276,7 +325,8 @@ sub env {
 
     if ($self->{events}{env_to_db}->status == -1){
         $event_error = 1;
-        print "event failure!\n";
+        print "event failure, restarting!\n";
+        $self->{events}{env_to_db}->restart;
     }
 
     my $ret = $self->db->env;
